b909b9ddc0f9dc1f77b68740e4ed10da954f346b
Miikka Värri <17232523+ruuti@users.noreply.github.com>
2019-03-31 19:55:20 +0200

    Minify CSS files when creating a  production release

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
 const withSass = require('@zeit/next-sass')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 module.exports = withSass({
-  minified:true,
+  webpack(config, options) {
+    if (config.mode === 'production') {
+      if (Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}))
+      }
+    }
+    return config
+  },
   poweredByHeader: false
 })
