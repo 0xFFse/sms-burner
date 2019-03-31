5567213f710eec71f58dea479fa1fee2612ef6ac
Miikka Värri <17232523+ruuti@users.noreply.github.com>
2019-03-31 17:54:25 +0200

    Renamed About page component to match pattern

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -4,9 +4,9 @@ import Footer from '../components/Footer';
 import "../styles.scss"
 
 /**
- * Home -page top level component
+ * About -page top level component
  */
-function Home() {
+function About() {
   return (
     <div className={'page-wrapper'}>
       <Head>
@@ -21,7 +21,7 @@ function Home() {
         <Header />
         <div className={'content-with-sidebar row'}>
         <p>
-            Fler och fler webbtj&auml;nster kr&auml;ver telefonnummer vid kontoregistrering. 
+            Fler och fler webbtj&auml;nster kr&auml;ver telefonnummer vid kontoregistrering.
             D&aring; ditt telefonnummer &auml;r unikt kan det enkelt anv&auml;ndas f&ouml;r att knyta ihop skuggprofiler och b&auml;ttre kartl&auml;gga ditt internetanv&auml;ndande.<br/>
             &nbsp;<br/>
             SMS burner togs fram som en tj&auml;nst f&ouml;r att anonymt l&aring;ta anv&auml;ndare kunna dela p&aring; telefonnummer vid kontoregistrering och v&auml;rna om den personliga integriteten.<br/>
@@ -35,4 +35,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default About;
