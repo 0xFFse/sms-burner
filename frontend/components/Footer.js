730774e8c67b409e67a05a61b9f84c10cdf288e9
Miikka Värri <17232523+ruuti@users.noreply.github.com>
2019-04-01 07:50:49 +0200

    rel="noopener" added to external links

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer>
       <p>
-        <Link href={'/about'}><a title={'Om'}>{'Om'}</a></Link> | <a target="_blank" href="https://github.com/0xFFse/sms-burner">&Ouml;ppen k&auml;llkod</a> | <a target="_blank" href="https://0xff.se">0xFF</a> | Ikoner fr&aring;n <a target="_blank" href="https://icons8.com/">Icons8.com</a>
+        <Link href={'/about'}><a title={'Om'}>{'Om'}</a></Link> | <a target="_blank" rel="noopener" href="https://github.com/0xFFse/sms-burner">&Ouml;ppen k&auml;llkod</a> | <a target="_blank" rel="noopener" href="https://0xff.se">0xFF</a> | Ikoner fr&aring;n <a target="_blank" rel="noopener" href="https://icons8.com/">Icons8.com</a>
       </p>
     </footer>
   );
