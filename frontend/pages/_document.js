0c64723113f3a2704e8058a510b4a07f2e0e8510
Magnus@0xFF <m0xffse.github@0xff.se>
2019-04-02 16:19:34 +0200

    First take on meta-tags

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -14,7 +14,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="sv">
-        <Head />
+        <Head>
+          <meta name="description" content="Telefon-verifikation med bibeh&aring;llen integritet. Ta emot SMS anonymt med tillf&auml;rlligt telefonnummer." />
+          <meta property="og:description" content="Telefon-verifikation med bibeh&aring;llen integritet. Ta emot SMS anonymt med tillf&auml;rlligt telefonnummer." />
+          <meta property="og:title" content="SMS-burner - Ta emot SMS anonymt" />
+          <meta property="og:type" content="website" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@smsburner" />
+          <meta name="twitter:title" content="SMS-burner - Ta emot SMS anonymt" />
+          <meta name="twitter:description" content="Telefon-verifikation med bibeh&aring;llen integritet. Ta emot SMS anonymt med tillf&auml;rlligt telefonnummer." />
+          <meta name="twitter:image" content="https://sms.0xff.se/static/dump.png" />
+          <meta property="og:image" content="https://sms.0xff.se/static/dump.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
