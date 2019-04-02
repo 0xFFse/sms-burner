00c4ea220d1ccf4fb07362e000fa0898ee5291f7
Miikka Värri <17232523+ruuti@users.noreply.github.com>
2019-04-02 17:03:10 +0200

    Adds section and article tags to about page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -9,16 +9,18 @@ const About = () => (
   <Layout>
     <div className={'container'}>
       <Header />
-      <div className={'content-with-sidebar row'}>
-        <p>
-            Fler och fler webbtj&auml;nster kr&auml;ver telefonnummer vid kontoregistrering.
-            D&aring; ditt telefonnummer &auml;r unikt kan det enkelt anv&auml;ndas f&ouml;r att knyta ihop skuggprofiler och b&auml;ttre kartl&auml;gga ditt internetanv&auml;ndande.<br/>
-            &nbsp;<br/>
-            SMS burner togs fram som en tj&auml;nst f&ouml;r att anonymt l&aring;ta anv&auml;ndare kunna dela p&aring; telefonnummer vid kontoregistrering och v&auml;rna om den personliga integriteten.<br/>
-            &nbsp;<br/>
-            Tj&auml;nsten utvecklades av eldsj&auml;lar under ett Hackathon hos <a target="_blank" href="https://0xff.se/">0xFF Cyber tech community</a> i Stockholm en kv&auml;ll i Mars 2019.
-        </p>
-      </div>
+      <section>
+        <article className={'content-with-sidebar row'}>
+          <p>
+              Fler och fler webbtj&auml;nster kr&auml;ver telefonnummer vid kontoregistrering.
+              D&aring; ditt telefonnummer &auml;r unikt kan det enkelt anv&auml;ndas f&ouml;r att knyta ihop skuggprofiler och b&auml;ttre kartl&auml;gga ditt internetanv&auml;ndande.<br/>
+              &nbsp;<br/>
+              SMS burner togs fram som en tj&auml;nst f&ouml;r att anonymt l&aring;ta anv&auml;ndare kunna dela p&aring; telefonnummer vid kontoregistrering och v&auml;rna om den personliga integriteten.<br/>
+              &nbsp;<br/>
+              Tj&auml;nsten utvecklades av eldsj&auml;lar under ett Hackathon hos <a target="_blank" href="https://0xff.se/">0xFF Cyber tech community</a> i Stockholm en kv&auml;ll i Mars 2019.
+          </p>
+        </article>
+      </section>
       <Footer />
     </div>
   </Layout>
