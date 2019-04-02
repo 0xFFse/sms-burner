315fcac8d2d2c141fed33f8b872a0cc441349c1c
Miikka Värri <17232523+ruuti@users.noreply.github.com>
2019-04-02 23:49:07 +0200

    header links wrapper around nav and ul

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -16,11 +16,35 @@ function Header() {
         </div>
       </div>
       <div className={'description'}>
-        <h1>Telefon-verifikation med bibeh&aring;llen integritet</h1>
+        <h1>
+          Telefon-verifikation med bibeh&aring;llen integritet
+        </h1>
       </div>
-      <div className={'navbar'}>
-        <Link href={'/about'}><a title={'Om'}>{'Om'}</a></Link> | <a target="_blank" rel="noopener" href="https://github.com/0xFFse/sms-burner">&Ouml;ppen k&auml;llkod</a> | <a target="_blank" rel="noopener" href="https://0xff.se">0xFF</a>
-      </div>
+      <nav className={'navbar'}>
+        <ul>
+          <li>
+            <Link href={'/about'}>
+              <a title={'Om'}>
+                {'Om'}
+              </a>
+            </Link>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener"
+              href="https://github.com/0xFFse/sms-burner"
+            >
+              &Ouml;ppen k&auml;llkod
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noopener" href="https://0xff.se">
+              0xFF
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
