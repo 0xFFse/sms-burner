490a095713e70d158bfa7c7cc0198c6eba1b1369
Magnus@0xFF <m0xffse.github@0xff.se>
2019-04-02 15:08:33 +0200

    PNG logo and nav on top

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <footer>
       <p>
-        <Link href={'/about'}><a title={'Om'}>{'Om'}</a></Link> | <a target="_blank" rel="noopener" href="https://github.com/0xFFse/sms-burner">&Ouml;ppen k&auml;llkod</a> | <a target="_blank" rel="noopener" href="https://0xff.se">0xFF</a> | Ikoner fr&aring;n <a target="_blank" rel="noopener" href="https://icons8.com/">Icons8.com</a>
+        <a target="_blank" rel="noopener" href="https://0xff.se">0xFF</a> | Ikoner fr&aring;n <a target="_blank" rel="noopener" href="https://icons8.com/">Icons8.com</a>
       </p>
     </footer>
   );
